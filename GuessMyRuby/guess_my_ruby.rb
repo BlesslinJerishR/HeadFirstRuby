cca4cec2c5cedb158b49f6909f060bc5db262420
Mastermind <mastermindx33@gmail.com>
2023-04-02 15:05:04 +0530

    __init__

---
+++ GuessMyRuby/guess_my_ruby.rb
@@ -0,0 +1,9 @@
+# Guess my Number Game
+# Developer : Blesslin Jerish R
+
+
+puts "Welcome to 'Get My Number!'"
+print "What's your name? "
+input = gets
+name = input.chomp
+puts "Welcome #{name}!"
